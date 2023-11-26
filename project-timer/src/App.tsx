43513c000a1d037afe8b837a1ef2f06a62321076
Frankdias92 <frankmcdias@gmail.com>
2023-11-26 02:27:21 -0300

    structuring the application

--- project-timer/src/App.tsx
+++ project-timer/src/App.tsx
@@ -1,12 +1,25 @@
+import { ThemeProvider } from "styled-components"
+import { Button } from "./components/Button"
+import { defaultTheme } from "./components/styles/themes/default"
+import { GlobalStyle } from "./components/styles/themes/global"
+
 
 
 export function App() {
 
 
   return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Project Timer</h1>
+
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+      <Button />
    
-    <h1>Project Timer</h1>
-   
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
